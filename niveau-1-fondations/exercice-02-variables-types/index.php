769fa6364afc86e8687cae30978bc6d6378d720b
MoDev228 <mb98402074@gmail.com>
2026-08-16 16:48:07 +0000

    Validation du deuxieme exercice

---
+++ niveau-1-fondations/exercice-02-variables-types/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$prenom = "Mohamed";
+$nom = "Boukari";
+$age = 44;
+$taille = 1.77;
+$estEtudiant = true;
+$ville = "Lomé";
+
+const PAYS = "Togo";
+
+echo $prenom . PHP_EOL;
+echo $nom . PHP_EOL;
+echo $age . PHP_EOL;
+echo $taille . PHP_EOL;
+echo $estEtudiant . PHP_EOL;
+echo $ville . PHP_EOL;
+echo PAYS . PHP_EOL;
+
+
+echo "Je suis $nom $prenom, j’ai $age ans, je mesure $taille mètre, j’habite à $ville" . PHP_EOL;
+
+
+var_dump($nom);
+var_dump($prenom);
+var_dump($age);
+var_dump($taille);
+var_dump($estEtudiant);
+var_dump($ville);
+var_dump(PAYS);
+
