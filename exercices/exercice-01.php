82bea9f191ceb1908b914897cfeca1fd77d17c1c
MoDev228 <mb98402074@gmail.com>
2026-08-27 13:35:29 +0000

    feat(exo-01): ajouter le profil utilisateur

---
+++ exercices/exercice-01.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Exercice 1 — Profil utilisateur
+ *
+ * Ce programme stocke et affiche les informations d'un utilisateur.
+ */
+
+// Informations de l'utilisateur
+$nom = "BOUKARI";
+$prenom = "Mohamed";
+$age = 27;
+$ville = "Lomé";
+$profession = "Développeur PHP";
+
+// Affichage des informations
+ echo "Bonjour, je m’appelle $nom $prenom." . PHP_EOL;
+echo "J’ai $age ans et j’habite à $ville." . PHP_EOL;
+echo "Je suis $profession." . PHP_EOL;
